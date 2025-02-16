cc184909e6db1d0efe7138387fbf90a071729766
Leo <prodopark15@github.com>
2025-02-16 23:25:09 +0900

    feat: loadingspinner 가져오기

---
+++ src/components/common/LoadingSpinner.jsx
@@ -0,0 +1,40 @@
+import styled, { keyframes } from 'styled-components';
+
+const spin = keyframes`
+  0% { transform: rotate(0deg); }
+  100% { transform: rotate(360deg); }
+`;
+
+const SpinnerContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  height: 400px;
+`;
+
+const Spinner = styled.div`
+  width: 50px;
+  height: 50px;
+  border: 5px solid #f3f3f3;
+  border-top: 5px solid #3b90f9;
+  border-radius: 50%;
+  animation: ${spin} 1s linear infinite;
+`;
+
+const LoadingText = styled.p`
+  margin-top: 20px;
+  color: #666;
+  font-size: 16px;
+`;
+
+function LoadingSpinner() {
+  return (
+    <SpinnerContainer>
+      <Spinner />
+      <LoadingText>AI가 여행 일정을 만들고 있어요...</LoadingText>
+    </SpinnerContainer>
+  );
+}
+
+export default LoadingSpinner;
