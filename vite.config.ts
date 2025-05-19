ac115c53eb3ec6004bd8435c06fcc895889c661d
projectkorea <prodopark15@gmail.com>
2025-05-19 21:31:15 +0900

    feat: migrate to vite #2

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,20 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import path from 'path';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  resolve: {
+    alias: [
+      { find: '@', replacement: path.resolve(__dirname, 'src') },
+      { find: '@components', replacement: path.resolve(__dirname, 'src/components') },
+      { find: '@common', replacement: path.resolve(__dirname, 'src/common') },
+      { find: '@styles', replacement: path.resolve(__dirname, 'src/styles') },
+      { find: '@hooks', replacement: path.resolve(__dirname, 'src/hooks') },
+      { find: '@store', replacement: path.resolve(__dirname, 'src/store') },
+      { find: '@pages', replacement: path.resolve(__dirname, 'src/pages') },
+      { find: '@data', replacement: path.resolve(__dirname, 'src/data') },
+    ],
+  },
+});
